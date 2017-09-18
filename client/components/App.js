25681b2d5208fcfd5dc0c8aff024d8fce5724fc4
lerning <joshuabenlerner@gmail.com>
2017-09-18 13:59:22 -0600

    instantiated server, setting up file architecture

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Toggles from './Toggles'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+
+const App = () => (
+  <div>
+    <NavBar />
+      <div className="container">
+          <AddTodo />
+          <VisibleTodoList />
+          <Toggles />
+      </div>
+  </div>
+)
+
+export default App
